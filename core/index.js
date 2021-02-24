5e1717943f35df8e6846f6e243c4c1768fb7d413
yonle <fledyonle@gmail.com>
2021-02-24 21:42:54 +0800

    Please read OpenRadio [Github](https://github.com/Yonle/openradio) release for changelog

--- core/index.js
+++ core/index.js
@@ -1,92 +1,89 @@
 // Openradio Core
-const sink = new Map();
-const ffprobe = require("node-ffprobe");
+const ffprobe = require("fluent-ffmpeg").ffprobe;
+const Throttle = require("throttle");
 const events = require("events");
-const CoreEvent = new events();
-var stream = null;var bps;
 
-CoreEvent.write = function (chunk) {
-	CoreEvent.emit('data', chunk);
-}
+function OpenRadio_Core () {
 
-sink.deleteAll = function deleteAll () {
-	sink.forEach((s, id) => {
-		sink.delete(id);
-	});
-}
+	const CoreEvent = new events();
+	const sink = new Map();
+	var stream = null;var bps;var Core = {};
 
-Core.playing = false;
-Core.ended = false;
-
-// Player
-async function Core(readable, bps) {
-        return Promise((res, rej) => {
-			if (!bps) {
-				ffprobe({ source: readable }).then(({ format }) => {
-					bps = format.bit_rate / 8;
-				}).catch(() => bps = 128000 / 8);
-			}
-                        stream = new Throttle(bps);
-                        stream.on("data", (chunk) => {
-				CoreEvent.write(chunk);
-                                sink.forEach((s, id) => {
-					try {
-                                                s.write(chunk, (error) => {
-                                                        if (error) {
-                                                                logs.push(`[${Date}] ${e}`);
-                                                                return sink.delete(id);
-                                                        }
-                                                });
-					} catch (error) {
-						sink.remove(id);
-					}
-                                });
-                        });
-
-                        stream.on("end", () => {
-                                CoreEvent.emit("end", null);
-				Core.ended = true;
-				Core.playing = false;
-                        });
-
-                        if (!readable||!readable.pipe) return rej(`Not readable stream`);
-                        readable.pipe(stream);
-			Core.playing = true;
-			Core.ended = false;
-                        return res(stream);
-        });
-}
-
-Core.pipe = function Player (writable) {
-	var id = Math.random().toString(36).slice(2);
-	if (!writable||!writable.write||typeof(writable) !== "WriteStream") {
-		throw new TypeError("Not a writable stream");
-		return false;
+	CoreEvent.write = function WriteBuffer (chunk) {
+		CoreEvent.emit('data', chunk);
 	}
-	sink.set(id, sink);
-}
 
-Core.on = CoreEvent.on;
+	sink.deleteAll = function deleteAll () {
+		sink.forEach((s, id) => {
+			sink.delete(id);
+		});
+	}
 
-Core.end = function End () {
-	return Promise ((res, rej) => {
-		if (stream) {
-			stream.end(res);
-			return true;
-		} else {
-			rej("Radio isn't started");
+	Core.playing = false;
+        Core.ended = false;
+	Core.end = null;
+
+	// Player
+	Core.play = async function ReadStream (readable, BytePerSecond) {
+		var bps = BytePerSecond;
+        	return new Promise ((res, rej) => {
+				if (!bps) {
+					ffprobe(readable, async (err, data) => {
+						if (err) {
+							bps = 128000 / 8;
+						} else {
+							bps = data.streams[0].bit_rate / 8;
+						}
+					})
+				}
+                	        stream = new Throttle(bps);
+                        	stream.on("data", (chunk) => {
+					CoreEvent.write(chunk);
+	                                sink.forEach((s, id) => {
+						try {
+                	                                s.write(chunk, (error) => {
+                        	                                if (error) {
+                                	                                logs.push(`[${Date}] ${e}`);
+                                        	                        return sink.delete(id);
+                                                        	}
+                                                	});
+						} catch (error) {
+							sink.delete(id);
+						}
+                        	        });
+	                        });
+
+        	                stream.on("end", () => {
+                        	        CoreEvent.emit("end", null);
+					Core.ended = true;
+					Core.playing = false;
+					Core.end = null;
+                	        });
+
+        	                if (!readable||!readable.pipe) return rej(`Not readable stream`);
+                	        readable.pipe(stream);
+				Core.playing = true;
+				Core.ended = false;
+				Core.end = stream.end;
+	                        return res(stream);
+	        });
+	}
+
+	Core.pipe = function Player (writable) {
+		var id = Math.random().toString(36).slice(2);
+		if (!writable||!writable.write||typeof(writable) !== "WriteStream") {
+			throw new TypeError("Not a writable stream");
 			return false;
 		}
-	});
+		sink.set(id, sink);
+		return id;
+	}
+
+	Core.on = CoreEvent.on;
+
+	Core.sink = sink;
+
+	return Core;
 }
 
-Core.sink = sink;
-
-Core.write = function WriteStream (chunk, callback) {
-	if (!stream) return null;
-	stream.write(chunk, callback);
-}
-
-Core.play = Core;
-
-module.exports = Core;
+module.exports = OpenRadio_Core;
