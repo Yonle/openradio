2b5dacfe1934111f0475b17087d884f3fe6a775e
Yonle <63401712+Yonle@users.noreply.github.com>
2021-04-11 20:48:38 +0800

    Fix known bugs

--- core/index.js
+++ core/index.js
@@ -35,7 +35,8 @@ function OpenRadio_Core(opt) {
             return new Error("BytePerSecond is Deprecated.");
         }
 
-        stream = readable.pipe(convert(opt)).on('error', e => Core.emit('error', e)).pipe(Throttle((opt.bitrate * 1000 || 96000) / 8));
+        stream = readable.pipe(convert(opt)).on('error', e => Core.emit('error', e)).pipe(Throttle(((() => { if (opt && opt.bitrate) opt.bitrate * 1000 })() || 96000
+        ) / 8));
         
         stream.on("data", (chunk) => {
             Core.emit("data", chunk);
